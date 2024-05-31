3d36ee72d75d214be3e5eb14c10f58ee5af3eef2
Vũ văn Phúc <phucvuvan63@gmail.com>
2024-06-01 00:39:12 +0700

    thêm code lab6

---
+++ Seat.kt
@@ -0,0 +1,3 @@
+package ph34858.laptrinhkotlin.lab6Ph34858
+
+data class Seat(var row: Char, val number: Int, var status: SeatStatus)
---
+++ StatusSeat.kt
@@ -0,0 +1,3 @@
+package ph34858.laptrinhkotlin.lab6Ph34858
+
+enum class SeatStatus { EMPTY, SELECTED, BOOKED, AISLE }
